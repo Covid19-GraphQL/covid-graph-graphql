b1339ccd6c16a537737ac7fc75219bb3c0d15af9
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2020-10-16 10:14:16 -0700

    fixes old broken database url

--- api/inferSchema.js
+++ api/inferSchema.js
@@ -5,7 +5,7 @@ import { createWriteStream } from 'fs';
 const writeStream = createWriteStream('./infered-schema.graphql');
 
 const driver = neo4j.driver(
-  process.env.COVID_GRAPH_NEO4J_URI || "bolt://covid.petesis.com:7687",
+  process.env.COVID_GRAPH_NEO4J_URI || "bolt://db.covidgraph.org:7687",
   neo4j.auth.basic(
     process.env.COVID_GRAPH_NEO4J_USER || "public",
     process.env.COVID_GRAPH_NEO4J_PASSWORD || "corona"
