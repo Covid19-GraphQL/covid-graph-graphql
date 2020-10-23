6abc0af72cffef3ab147f08add24327ddabc4a23
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2020-10-22 22:55:40 -0700

    Update graphql-schema.js

--- api/src/graphql-schema.js
+++ api/src/graphql-schema.js
@@ -1,12 +1,7 @@
-import {
-  mergeTypeDefs,
-  mergeResolvers,
-  loadFilesSync
-} from 'graphql-tools';
-import path from 'path';
+import { mergeTypeDefs, mergeResolvers, loadFilesSync } from 'graphql-tools'
 
-const dataSourceTypes = loadFilesSync(path.join(__dirname, './schemas/**/typeDefs/*.js'));
-export const typeDefs = mergeTypeDefs(dataSourceTypes);
+const dataSourceTypes = loadFilesSync('./src/schemas/**/typeDefs/*.js')
+export const typeDefs = mergeTypeDefs(dataSourceTypes)
 
-const dataSourceResolvers = loadFilesSync(path.join(__dirname, './schemas/**/resolvers/*.js'));
-export const resolvers = mergeResolvers(dataSourceResolvers);
+const dataSourceResolvers = loadFilesSync('./src/schemas/**/resolvers/*.js')
+export const resolvers = mergeResolvers(dataSourceResolvers)
