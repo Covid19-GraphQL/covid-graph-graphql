a8c288e1f6923ffa1919306a4e4fcc62195edbc3
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2020-10-24 16:06:03 -0700

    Update client.js

--- api/src/schemas/client.js
+++ api/src/schemas/client.js
@@ -5,14 +5,17 @@ import dotenv from 'dotenv'
 // set environment variables from .env
 dotenv.config()
 
+const VERCEL_URI = `https://covid-graph-graphql.now.sh/`
+
+// Uncomment for local testing
 // Specify host, port and path for GraphQL endpoint
-const port = process.env.GRAPHQL_SERVER_PORT || 4001
-const host = process.env.GRAPHQL_SERVER_HOST || '0.0.0.0'
-const path = process.env.GRAPHQL_SERVER_PATH || '/graphql'
+// const port = process.env.GRAPHQL_SERVER_PORT || 4001
+// const host = process.env.GRAPHQL_SERVER_HOST || '0.0.0.0'
+// const path = process.env.GRAPHQL_SERVER_PATH || '/graphql'
 
 // Used by Ava tests
 export const client = new ApolloClient({
-  link: new HttpLink({ uri: `http://${host}:${port}${path}`, fetch: fetch }),
+  link: new HttpLink({ uri: VERCEL_URI, fetch: fetch }),
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
